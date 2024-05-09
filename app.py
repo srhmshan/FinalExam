00625fc65328037aca3826625a245037a4ad69ac
Shan <143104625+srhmshan@users.noreply.github.com>
2024-05-09 22:33:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,48 +1,37 @@
 import streamlit as st
 import tensorflow as tf
-
-@st.cache_resource
-def load_model():
-  model=tf.keras.models.load_model('plant_classifier.hdf5')
-  return model
-model=load_model()
-st.write("""
-# Plant Leaf Detection System by Jonathan"""
-)
-file=st.file_uploader("Choose plant photo from computer",type=["jpg","png"])
-
-import cv2
-from PIL import Image,ImageOps
+from PIL import Image, ImageOps
 import numpy as np
-def import_and_predict(image_data,model):
-    size=(64,64)
-    image=ImageOps.fit(image_data,size)
-    img=np.asarray(image)
-    img_reshape=img[np.newaxis,...]
-    prediction=model.predict(img_reshape)
-    return prediction
-if file is None:
-    st.text("Please upload an image file")
-else:
-    image=Image.open(file)
-    st.image(image,use_column_width=True)
-    prediction=import_and_predict(image,model)
-    class_names=['Alpinia Galanga (Rasna)','Amaranthus Viridis (Arive-Dantu)',
-                 'Artocarpus Heterophyllus (Jackfruit)',
-                 'Azadirachta Indica (Neem)','Basella Alba (Basale)',
-                 'Brassica Juncea (Indian Mustard)','Carissa Carandas (Karanda)',
-                 'Citrus Limon (Lemon)','Ficus Auriculata (Roxburgh fig)',
-                 'Ficus Religiosa (Peepal Tree)','Hibiscus Rosa-sinensis',
-                 'Jasminum (Jasmine)','Mangifera Indica (Mango)',
-                 'Mentha (Mint)','Moringa Oleifera (Drumstick)',
-                 'Muntingia Calabura (Jamaica Cherry-Gasagase)',
-                 'Murraya Koenigii (Curry)', 'Nerium Oleander (Oleander)',
-                 'Nyctanthes Arbor-tristis (Parijata)','Ocimum Tenuiflorum (Tulsi)',
-                 'Piper Betle (Betel)','Plectranthus Amboinicus (Mexican Mint)',
-                 'Pongamia Pinnata (Indian Beech)','Psidium Guajava (Guava)',
-                 'Punica Granatum (Pomegranate)','Santalum Album (Sandalwood)',
-                 'Syzygium Cumini (Jamun)','Syzygium Jambos (Rose Apple)',
-                 'Tabernaemontana Divaricata (Crape Jasmine)',
-                 'Trigonella Foenum-graecum (Fenugreek)']
-    string="OUTPUT : "+class_names[np.argmax(prediction)]
-    st.success(string)
+
+@st.cache(allow_output_mutation=True)
+def load_model():
+    model = tf.keras.models.load_model('tsdr_model.h5')
+    return model
+
+model = load_model()
+
+classes = {5: 'Speed limit (60km/h)', 11: 'No Left Turn', 17: 'No Blowing of Horns', 
+           30: 'Bike Lane Ahead', 44: 'Side Road Junction Ahead (Left)'}
+
+st.title('Traffic Sign Classification by Group 6')
+
+uploaded_file = st.file_uploader("Upload a traffic sign image", type=["jpg", "jpeg", "png"])
+
+def preprocess_image(image):
+    image = image.resize((224, 224))
+    image = np.asarray(image) / 255.0
+    image = np.expand_dims(image, axis=0)
+    return image
+
+if uploaded_file is not None:
+    st.image(uploaded_file, caption='Uploaded Image.', use_column_width=True)
+    image = Image.open(uploaded_file)
+    image = preprocess_image(image)
+
+    prediction = model.predict(image)
+    predicted_class_index = np.argmax(prediction)
+    if predicted_class_index in classes:
+        predicted_class_label = classes[predicted_class_index]
+        st.success(f"Prediction: {predicted_class_label}")
+    else:
+        st.error("Unknown traffic sign class")
